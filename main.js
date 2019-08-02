a6f2e54744840b0fe76a30622b7c84f24e2bdbec
brandoncasasnovas <haphazerdous@aol.com>
2019-08-01 20:35:23 -0400

    doing last commit with css exclusively and removed js function

--- main.js
+++ main.js
@@ -209,10 +209,7 @@ const generateCardBlock = (cardParams) => {
                     } else if (effects.length == 1){
                         description.innerHTML += "Description: " + effects[0];
                     }
-                // prevents right click menu of image so on mobile if a user clicks to zoom, they don't get forced into default save image menu
-                $("#card_image").on("contextmenu",function(e){
-                return false;
-});   
+
                 }
             }
         }
